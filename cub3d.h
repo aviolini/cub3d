ce80e8ee95e5b1a2ff6df376c56e8beb55e4a55c
aviolini <arrigo.violini@gmail.com>
2021-02-28 19:57:57 +0100

    o

---
+++ cub3d.h
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aviolini <aviolini@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/28 16:57:26 by aviolini          #+#    #+#             */
+/*   Updated: 2021/02/28 17:00:12 by aviolini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <mlx.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+typedef struct  b_data
+{
+    void        *img;
+    char        *addr;
+    int         bits_per_pixel;
+    int         line_length;
+    int         endian;
+	int			size_pixel;
+	int 		xn;
+	int 		yn;
+}               img_data;
+
+typedef struct	a_data
+{
+	void		*mlx;
+	void		*win;
+	img_data	*img_s;
+}				win_data;
+
+typedef struct c_data
+{
+
+}				map_data;
+
+#endif
--- main.c
+++ main.c
@@ -1,38 +1,131 @@
-#include <mlx.h>
-#include <stdio.h>
-
-typedef struct	s_data
-{
-	void		*mlx;
-	void		*win;
-}				t_data;
+#include "cub3d.h"
 
 int		create_trgb(int t, int r, int g, int b)
 {
 	return(t << 24 | r << 16 | g << 8 | b);
 }
 
-int		key_hook(int keycode, t_data *s )
+void            my_mlx_pixel_put(img_data *img, int x, int y, int color)
 {
-	(void)s;
+    char    *dst;
+
+    dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+    *(unsigned int*)dst = color;
+}
+
+void            my_mlx_pixel_put2(img_data *img, int x, int y, int color)
+{
+    char    *dst;
+	int		i = -1, z = -1;
+
+	while (++i <= img->size_pixel && -2 < (z = -1))
+		while (++z <= img->size_pixel)
+		{
+    		dst = img->addr + ((i + y) * img->line_length + (x + z) * (img->bits_per_pixel / 8));
+    		*(unsigned int*)dst = color;
+		}
+}
+
+int		key_hook(int keycode, win_data *win)
+{
+	if (keycode == 53)
+		exit(0);
+	if (keycode == 124)
+	{
+		my_mlx_pixel_put2(win->img_s, win->img_s->xn, win->img_s->yn, 0x00FF0000);
+		mlx_put_image_to_window(win->mlx, win->win, win->img_s->img, 20, 20);
+	}
 	printf("%d\n", keycode);
 	return 0;
 }
 
-int     main(void)
+int             main(void)
 {
-	t_data	s;
-	int x = 0;
+    win_data    	win;
+    img_data		img;
+	char			buff[5001];
+	char			map[14][1000];
+	int				fd, x = 0, y = 0, r = 0, i = 0, j = 0,z = 0;
+	unsigned int	color;
+	win.img_s = &img;
+    win.mlx = mlx_init();
+	win.win = mlx_new_window(win.mlx, 800, 600, "Hello world!");
+	img.img = mlx_new_image(win.mlx, 600, 480);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+                                 &img.endian);
+	while (y++ < 1000 && -1 < (x = 0))
+		while (x++ < 1920)
+			mlx_pixel_put(win.mlx, win.win, x, y, 0x00FFFFFF);
+	printf("line_lenght: %d\n", img.line_length);
+	printf("line_lenght/4: %d\n", img.line_length/4);
+	printf("bits_per_pixel: %d\n", img.bits_per_pixel);
 
-	int z = create_trgb(0,0,1,0);
+	img.size_pixel = 10;
+	color = 255;
+	printf("color: %u\n", color);
+	y = -1;
+	fd = open("map_clean.cub", O_RDONLY, 0);
+	r = read(fd, buff, 5000);
+	buff[5000] = '\0';
+	//map[14][0] = NULL;
+	printf("buff\n%s\n",buff);
+	while (j < 14)
+	{
+		while (buff[i] != '\n')
+		{
+			if (buff[i] == ' ')
+				map[j][z] = ' ';
+				//i++;
+			else if (buff[i] == '1')
+		 		map[j][z] = '1';
+			else if (buff[i] == '0')
+				map[j][z] = '0';
+			else if (buff[i] == '2')
+				map[j][z] = '2';
+			else if (buff[i] == 'N')
+				map[j][z] = 'N';
+			i++;
+			z++;
+		}
+		map[j][z++] = '\0';
+		i++;
+		j++;
+		z = 0;
+	}
+	j = 0;
 
-	printf("colour:%i\n",z);
-    s.mlx = mlx_init();
-    s.win = mlx_new_window(s.mlx, 100, 100, "Hello world!");
-	while (x++ < 50)
-		mlx_pixel_put(s.mlx, s.win, x,50, 0x00FFFFFF);
-	mlx_key_hook(s.win, key_hook, &s);
-	mlx_loop(s.mlx);
+	while ( j < 14)
+		printf("map%s\n",map[j++]);
 
-
+		j = 0;
+		z = 0;
+		x = img.size_pixel;
+		y = img.size_pixel;
+	//}
+	while (j < 14)
+	{
+		while ( map[j][z] != '\0')
+		{
+			if (map[j][z] == '1')
+    			my_mlx_pixel_put2(&img, x, y, color);
+			if (map[j][z] == 'N')
+			{
+				img.xn = x;
+				img.yn = y;
+	    		my_mlx_pixel_put2(&img, x, y, 0x00FFFFFF);
+			}
+			if (map[j][z] == '2')
+		    	my_mlx_pixel_put2(&img, x, y, 0x0000FFFF);
+			x = x + img.size_pixel;
+			z++;
+			//printf("color: %u\n", color);
+		}
+		j++;
+		z = 0;
+		x = img.size_pixel;
+		y = y + img.size_pixel;
+	}
+    mlx_put_image_to_window(win.mlx, win.win, img.img, 20, 20);
+	mlx_key_hook(win.win, key_hook, &win);
+    mlx_loop(win.mlx);
 }
