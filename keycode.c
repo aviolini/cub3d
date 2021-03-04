e60732a29556ac8615874a34fb84ff77eb01a1a0
aviolini <arrigo.violini@gmail.com>
2021-03-04 12:08:56 +0100

    o

--- keycode.c
+++ keycode.c
@@ -6,7 +6,7 @@
 /*   By: aviolini <aviolini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/01 19:09:02 by aviolini          #+#    #+#             */
-/*   Updated: 2021/03/04 11:28:45 by aviolini         ###   ########.fr       */
+/*   Updated: 2021/03/04 12:08:37 by aviolini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,21 @@ void	ft_keycode_up(win_data *win)
 {
 	ft_destroy_img(win);
 	ft_build_image(win,0);
-	if (*(win->img_s->addr + ((win->map_s->posy - 1) * win->img_s->line_length +
-			win->map_s->posx * (win->img_s->bits_per_pixel / 8))) == 0)
-		{
-			win->map_s->posy--;
-			my_mlx_pixel_put(win->img_s, win->map_s->posx, win->map_s->posy , 0x00FFFFFF);
-		}
+
+	int posx, posy;
+	//int i = 0;
+	posx = win->map_s->posx;
+	posy = win->map_s->posy;
+	posy -= pow(win->map_s->diry,2)* win->map_s->diry;
+	posx += pow(win->map_s->dirx,2)* win->map_s->dirx;
+
+	if (*(win->img_s->addr + ((posy) * win->img_s->line_length +
+	posx * (win->img_s->bits_per_pixel / 8))) == 0)
+	{
+		win->map_s->posy-=pow(win->map_s->diry,2)* win->map_s->diry;
+		win->map_s->posx+=pow(win->map_s->dirx,2)* win->map_s->dirx;
+		my_mlx_pixel_put(win->img_s, win->map_s->posx, win->map_s->posy , 0x00FFFFFF);
+	}
 	else
 		my_mlx_pixel_put(win->img_s, win->map_s->posx, win->map_s->posy  , 0x00FFFFFF);
 }
@@ -29,12 +38,21 @@ void	ft_keycode_down(win_data *win)
 {
 	ft_destroy_img(win);
 	ft_build_image(win,0);
-	if (*(win->img_s->addr + ((win->map_s->posy + 1) * win->img_s->line_length +
-			win->map_s->posx * (win->img_s->bits_per_pixel / 8))) == 0)
-		{
-			win->map_s->posy++;
-			my_mlx_pixel_put(win->img_s, win->map_s->posx, win->map_s->posy , 0x00FFFFFF);
-		}
+
+	int posx, posy;
+	//int i = 0;
+	posx = win->map_s->posx;
+	posy = win->map_s->posy;
+	posy += pow(win->map_s->diry,2)* win->map_s->diry;
+	posx -= pow(win->map_s->dirx,2)* win->map_s->dirx;
+
+	if (*(win->img_s->addr + ((posy) * win->img_s->line_length +
+	posx * (win->img_s->bits_per_pixel / 8))) == 0)
+	{
+		win->map_s->posy+=pow(win->map_s->diry,2)* win->map_s->diry;
+		win->map_s->posx-=pow(win->map_s->dirx,2)* win->map_s->dirx;
+		my_mlx_pixel_put(win->img_s, win->map_s->posx, win->map_s->posy , 0x00FFFFFF);
+	}
 	else
 		my_mlx_pixel_put(win->img_s, win->map_s->posx, win->map_s->posy , 0x00FFFFFF);
 }
