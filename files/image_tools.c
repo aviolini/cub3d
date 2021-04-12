0b995f79e79d74e7f22c0fc01b64e60dc0d34f8e
Arrigo Violini <aviolini@e3r4p7.42roma.it>
2021-04-12 18:36:47 +0200

    norminette image_tools.c

--- files/image_tools.c
+++ files/image_tools.c
@@ -6,128 +6,105 @@
 /*   By: aviolini <aviolini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/15 14:37:31 by aviolini          #+#    #+#             */
-/*   Updated: 2021/04/12 10:12:13 by aviolini         ###   ########.fr       */
+/*   Updated: 2021/04/12 18:36:02 by aviolini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "../include/cub3d.h"
-
 
 void	new_minimap_image(t_window *win, t_image *image)
 {
-	image->img = mlx_new_image(win->mlx, win->settings.mapW * SCALE, win->settings.mapH * SCALE);
-	image->addr = mlx_get_data_addr(image->img, &image->bits_per_pixel,
-		&image->line_length, &image->endian);
-	//win->settings.minimap_def = 1;
+	image->img = mlx_new_image(win->mlx, win->settings.mapW * SCALE, \
+		 						win->settings.mapH * SCALE);
+	image->addr = mlx_get_data_addr(image->img, &image->bits_per_pixel, \
+								&image->line_length, &image->endian);
 }
 
 void	new_image(t_window *win, t_image *image)
 {
-	image->img = mlx_new_image(win->mlx, win->settings.winW, win->settings.winH);
-	image->addr = mlx_get_data_addr(image->img, &image->bits_per_pixel,
-		&image->line_length, &image->endian);
+	image->img = mlx_new_image(win->mlx, win->settings.winW, \
+								win->settings.winH);
+	image->addr = mlx_get_data_addr(image->img, &image->bits_per_pixel, \
+								&image->line_length, &image->endian);
 	win->settings.img_def = 1;
 }
 
 void	my_mlx_pixel_put(t_image *img, int x, int y, int color)
 {
-    char    *dst;
-    dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
-    *(unsigned int*)dst = color;
+	char	*dst;
+
+	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
 }
 
-
-void	column(t_window *win, t_image *img,int x,int orientation)
+void	column(t_window *win, t_image *img, int x, int orientation)
 {
-	//double perpdist;
-	//double h;
-	char *dst;
-	int i;
-	unsigned int color;
-	//double walltopy = 0, wallbottomy = 0;
+	char			*dst;
+	int				i;
+	unsigned int	color;
 
-//	distprojplane = (win->settings.winW / 2) / tan(FOV / 2);
-
-	win->draw.perp_distance =  win->ray.distance[x] * (cos(win->ray.angle - win->player.angle));
-	win->draw.h_object = 1 / win->draw.perp_distance * win->draw.dist_proj_plane;
-//	h = win->settings.winH/perpdist;
-
-
+	win->draw.perp_distance = win->ray.distance[x] * \
+								(cos(win->ray.angle - win->player.angle));
+	win->draw.h_object = 1 / win->draw.perp_distance * \
+								win->draw.dist_proj_plane;
 	win->draw.start_topY = win->settings.winH / 2 - win->draw.h_object / 2;
 	win->draw.start_topY = win->draw.start_topY < 0 ? 0 : win->draw.start_topY;
 	win->draw.end_bottomY = win->settings.winH / 2 + win->draw.h_object / 2;
 	win->draw.end_bottomY = win->draw.end_bottomY > win->settings.winH ? win->settings.winH : win->draw.end_bottomY;
-	//char *dst;
 	i = win->draw.start_topY;
-	//int k = 0;
-
-
 	while ((i) < win->draw.end_bottomY - 1)
 	{
-
-	//int offsetY = (int)(64/h*k++);
-	win->draw.offsetY = (int)fabs((i + (win->draw.h_object / 2) - (win->settings.winH / 2))*win->textures[orientation].texH/win->draw.h_object);
-	color = *(win->textures[orientation].addr + ((int)(win->draw.offsetY)*win->textures[orientation].texH +
-	(int)((win->ray.indexTex-(int)win->ray.indexTex)*win->textures[orientation].texW)));
-
-    		dst = img->addr + ((int)(i++) * img->line_length +
-			(int)(x) * (img->bits_per_pixel / 8));
-			//if (color >= 4278190080)
-			//	dst = 255;
-		//else
-				*(unsigned int*)dst = color;
+		win->draw.offsetY = (int)fabs((i + (win->draw.h_object / 2) - \
+				(win->settings.winH / 2)) * \
+				win->textures[orientation].texH / win->draw.h_object);
+		color = *(win->textures[orientation].addr + \
+			((int)(win->draw.offsetY) * win->textures[orientation].texH + \
+			(int)((win->ray.indexTex - (int)win->ray.indexTex) * \
+				win->textures[orientation].texW)));
+		dst = img->addr + ((int)(i++) * img->line_length + \
+				(int)(x) * (img->bits_per_pixel / 8));
+		*(unsigned int *)dst = color;
 	}
 }
 
-int open_texture(t_window *win, int i, char *path)
+int	open_texture(t_window *win, int i, char *path)
 {
-	win->textures[i].tex = mlx_xpm_file_to_image(win->mlx,path,&win->textures[i].texW,&win->textures[i].texH);
+	win->textures[i].tex = mlx_xpm_file_to_image(win->mlx, path, \
+							&win->textures[i].texW, &win->textures[i].texH);
 	if (!win->textures[i].tex)
 		return (0);
 	return (1);
 }
 
-int		init_textures(t_window *win)
+int	init_textures(t_window *win)
 {
-	int i;
-	int useless_but_necessary_box[3];
+	int	i;
+	int	useless_but_necessary_box[3];
 
 	i = -1;
-	if ((!open_texture(win, ++i, win->settings.east_texture)) ||
-			(!open_texture(win, ++i, win->settings.north_texture)) ||
-			(!open_texture(win, ++i, win->settings.west_texture)) ||
-			(!open_texture(win, ++i, win->settings.south_texture)) ||
+	if ((!open_texture(win, ++i, win->settings.east_texture)) || \
+			(!open_texture(win, ++i, win->settings.north_texture)) || \
+			(!open_texture(win, ++i, win->settings.west_texture)) || \
+			(!open_texture(win, ++i, win->settings.south_texture)) || \
 			(!open_texture(win, ++i, win->settings.sprite_texture)))
 		return (0);
-
-	//if ((!(win->texture[0].tex = mlx_xpm_file_to_image(win->mlx,win->settings.east_texture,&win->texture[0].texW,&win->texture[0].texH)))
-	//|| (!(win->texture[1].tex = mlx_xpm_file_to_image(win->mlx,win->settings.north_texture,&win->texture[1].texW,&win->texture[1].texH)))
-	//|| (!(win->texture[2].tex = mlx_xpm_file_to_image(win->mlx,win->settings.west_texture,&win->texture[2].texW,&win->texture[2].texH)))
-	//|| (!(win->texture[3].tex = mlx_xpm_file_to_image(win->mlx,win->settings.south_texture,&win->texture[3].texW,&win->texture[3].texH)))
-	//|| (!(win->texture[4].tex = mlx_xpm_file_to_image(win->mlx,win->settings.sprite_texture,&win->texture[4].texW,&win->texture[4].texH))))
-	//		return (0);
 	i = -1;
 	while (++i < 5)
-		{
-			//win->textures[i].addr = malloc
-		/*	if(!(win->textures[i].addr =(int *)mlx_get_data_addr(win->textures[i].tex, &win->textures[i].bits_per_pixel,
-				&win->textures[i].line_length, &win->textures[i].endian)))
-					return (0);
-		*/	if(!(win->textures[i].addr =(int *)mlx_get_data_addr(win->textures[i].tex, &useless_but_necessary_box[0],
-				&useless_but_necessary_box[1], &useless_but_necessary_box[2])))
-					return (0);
-		//	mlx_put_image_to_window(win->mlx, win->win, win->textures[i].tex, 20 + x, 20);
-		//	x += 65;
-		//	print_tex(win->textures[i]);
-		}
+	{
+		if (!(win->textures[i].addr = \
+							(int *)mlx_get_data_addr(win->textures[i].tex, \
+							&useless_but_necessary_box[0], \
+							&useless_but_necessary_box[1], \
+							&useless_but_necessary_box[2])))
+			return (0);
+	}
 	return (1);
 }
 
 void	view_background(t_image *view, t_settings *settings)
 {
-	unsigned int				x;
-	unsigned int				y;
+	unsigned int	x;
+	unsigned int	y;
 	int				z;
 	unsigned int	color;
 
@@ -146,9 +123,7 @@ void	view_background(t_image *view, t_settings *settings)
 				x++;
 			}
 			y++;
-
 		}
-
 		color = settings->floor_color;
 		y--;
 	}
@@ -156,22 +131,23 @@ void	view_background(t_image *view, t_settings *settings)
 
 void	set_right_resolution(t_window *win)
 {
-	int myresx;
-	int myresy;
+	int	myresx;
+	int	myresy;
 
 	mlx_get_screen_size(win->mlx, &myresx, &myresy);
-	printf (" my res ; %d \t %d\n ",myresx, myresy);
 	if((int)win->settings.winW > myresx)
 		win->settings.winW = myresx;
 	if((int)win->settings.winH > myresy)
 		win->settings.winH = myresy;
-	if ((int)win->settings.winW < MIN_RES_W || (int)win->settings.winH < MIN_RES_H)
+	if ((int)win->settings.winW < MIN_RES_W || \
+								(int)win->settings.winH < MIN_RES_H)
 	{
 		win->settings.winW = MIN_RES_W;
 		win->settings.winH = MIN_RES_H;
 	}
 	win->draw.dist_proj_plane = (win->settings.winW / 2) / tan(FOV / 2);
 }
+
 void	print_intersection(t_window *win, double intersectionX, double intersectionY)
 {
 	if(win->settings.minimap_def)
