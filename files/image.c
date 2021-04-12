b75c80dc05b2d78505306a1bc411a2fae0a4e6d0
Arrigo Violini <aviolini@e3r4p7.42roma.it>
2021-04-12 18:11:46 +0200

    norminette image.c

--- files/image.c
+++ files/image.c
@@ -6,7 +6,7 @@
 /*   By: aviolini <aviolini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/19 10:36:22 by aviolini          #+#    #+#             */
-/*   Updated: 2021/04/12 18:05:18 by aviolini         ###   ########.fr       */
+/*   Updated: 2021/04/12 18:11:28 by aviolini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 int	build_view(t_window *win)
 {
 	new_minimap_image(win, &win->world);
-	if(win->settings.minimap_def)
-		if(!build_world(&win->world, win->settings.map, &win->player))
+	if (win->settings.minimap_def)
+		if (!build_world(&win->world, win->settings.map, &win->player))
 			return (0);
 	view_background(&win->view, &win->settings);
 	image(win);
-	if(win->settings.minimap_def)
-	miniray(win);
+	if (win->settings.minimap_def)
+		miniray(win);
 	sprite(win);
 	free(win->ray.distance);
 	if (win->settings.save == 0)
 	{
-		mlx_put_image_to_window(win->mlx, win->win, win->view.img, 05, 0);
-		if(win->settings.minimap_def)
+		mlx_put_image_to_window(win->mlx, win->win, win->view.img, 00, 0);
+		if (win->settings.minimap_def)
 			mlx_put_image_to_window(win->mlx, win->win, win->world.img, 20, 20);
 		mlx_do_sync(win->mlx);
 		mlx_destroy_image(win->mlx, win->world.img);
@@ -43,7 +43,7 @@ int	image(t_window *win)
 	int				orientation;
 
 	i = 0;
-	win->ray.angle = win->player.angle + FOV/2;
+	win->ray.angle = win->player.angle + FOV / 2;
 	win->ray.distance = (double *)malloc(sizeof(double) * win->settings.winW);
 	while (i < win->settings.winW)
 	{
@@ -51,8 +51,9 @@ int	image(t_window *win)
 		win->ray.dirY = -sin(win->ray.angle);
 		check_hor_inters(&win->settings, win->player, &win->ray);
 		check_ver_inters(&win->settings, win->player, &win->ray);
-		orientation = set_distance_and_wall_orientation(win,win->player,&win->ray,i);
-		column(win, &win->view,i,orientation);
+		orientation = set_distance_and_wall_orientation(win, win->player, \
+			 											&win->ray, i);
+		column(win, &win->view, i, orientation);
 		win->ray.angle -= FOV / win->settings.winW;
 		i++;
 	}
@@ -65,7 +66,7 @@ void	check_hor_inters(t_settings *settings, t_player player, t_ray *ray)
 	double	horX;
 	double	horY;
 
-	if(ray->dirY < 0)
+	if (ray->dirY < 0)
 	{
 		roundy = 0;
 		ray->value = 1;
@@ -75,22 +76,21 @@ void	check_hor_inters(t_settings *settings, t_player player, t_ray *ray)
 		roundy = 1;
 		ray->value = -1;
 	}
-
 	horY = floor(player.posY) + roundy;
-	horX = player.posX + (fabs((player.posY) -(horY))
-				/ (ray->value * tan(ray->angle)));
-	while (floor(horX) >= 0 && floor(horX) < settings->mapW &&
+	horX = player.posX + (fabs((player.posY) -(horY)) / \
+			(ray->value * tan(ray->angle)));
+	while (floor(horX) >= 0 && floor(horX) < settings->mapW && \
 			(settings->map[(int)floor(horY)][(int)floor(horX)] != ' '))
 	{
-
-		if (settings->map[(int)floor((horY) + (roundy - 1))][(int)floor(horX)] == '1')
+		if (settings->map[(int)floor((horY) + (roundy - 1))] \
+							[(int)floor(horX)] == '1')
 		{
 			ray->horX = horX;
 			ray->horY = horY;
 			return ;
 		}
 		horY -= ray->value;
-		horX += ray->value/tan(ray->angle);
+		horX += ray->value / tan(ray->angle);
 	}
 	ray->horX = MY_MAX_LIMIT;
 	ray->horY = MY_MAX_LIMIT;
@@ -102,7 +102,7 @@ void	check_ver_inters(t_settings *settings, t_player player, t_ray *ray)
 	double	verX;
 	double	verY;
 
-	if(ray->dirX < 0)
+	if (ray->dirX < 0)
 	{
 		roundx = 0;
 		ray->value = 1;
@@ -112,22 +112,22 @@ void	check_ver_inters(t_settings *settings, t_player player, t_ray *ray)
 		roundx = 1;
 		ray->value = -1;
 	}
-		verX = floor(player.posX / 1) * 1 + roundx;
-		verY = player.posY + (fabs(player.posX - verX))
-		* (ray->value * tan(ray->angle));
-		while((floor(verY) >= 0 && floor(verY) < settings->mapH) &&
-				(settings->map[(int)floor(verY)][(int)floor(verX)] != ' '))
+	verX = floor(player.posX) + roundx;
+	verY = player.posY + (fabs(player.posX - verX)) * \
+							(ray->value * tan(ray->angle));
+	while ((floor(verY) >= 0 && floor(verY) < settings->mapH) && \
+			(settings->map[(int)floor(verY)][(int)floor(verX)] != ' '))
+	{
+		if (settings->map[(int)floor(verY)] \
+							[(int)floor(verX + (roundx - 1))] == '1')
 		{
-
-			if (settings->map[(int)floor(verY)][(int)floor(verX + (roundx - 1) )] == '1')
-			{
-				ray->verX = verX;
-				ray->verY = verY;
-				return ;
-			}
-			verX -= ray->value ;
-			verY += ray->value *tan(ray->angle);
-		 }
+			ray->verX = verX;
+			ray->verY = verY;
+			return ;
+		}
+		verX -= ray->value ;
+		verY += ray->value * tan(ray->angle);
+	}
 	ray->verX = MY_MAX_LIMIT;
 	ray->verY = MY_MAX_LIMIT;
 }
